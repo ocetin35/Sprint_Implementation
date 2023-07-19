b150791f60314d29882604d26e396cb1e93210a5
Moses <gichchege@gmail.com>
2023-07-18 17:22:19 -0700

    Updated pinbar step definitions

---
+++ src/test/java/com/V_Track_Project/step_definitions/Pinbar_Step_Definitions.java
@@ -0,0 +1,36 @@
+package com.V_Track_Project.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Pinbar_Step_Definitions {
+    @Given("User is on the home")
+    public void userIsOnTheHome() {
+    }
+
+    @And("User clicks on any page on the home page")
+    public void userClicksOnAnyPageOnTheHomePage() {
+    }
+
+    @When("User clicks on the Pinbar icon")
+    public void userClicksOnThePinbarIcon() {
+    }
+
+    @Then("User sees desired page highlighted in the header section in yellow")
+    public void userSeesDesiredPageHighlightedInTheHeaderSectionInYellow() {
+    }
+
+    @Given("User is on the homepage")
+    public void userIsOnTheHomepage() {
+    }
+
+    @When("User hovers on that specific page")
+    public void userHoversOnThatSpecificPage() {
+    }
+
+    @Then("User sees expected image source")
+    public void userSeesExpectedImageSource() {
+    }
+}
