c0be161feb70493e927d03086d6c7a64435d6108
hasanakbba <hsnakbba@gmail.com>
2023-07-20 23:08:44 -0400

    LastChanges Made to solve LoginStepDefs

--- src/test/java/com/V_Track_Project/step_definitions/LoginStepDefs.java
+++ src/test/java/com/V_Track_Project/step_definitions/LoginStepDefs.java
@@ -1,6 +1,6 @@
 package com.V_Track_Project.step_definitions;
 
-import com.V_Track_Project.pages.PinbarPage;
+
 import com.V_Track_Project.pages.R_VytrackLoginPage;
 import com.V_Track_Project.utilities.BrowserUtils;
 import com.V_Track_Project.utilities.ConfigurationReader;
@@ -20,7 +20,7 @@ import java.time.Duration;
 
 public class LoginStepDefs {
     R_VytrackLoginPage vytrackLoginPage = new R_VytrackLoginPage();
-    PinbarPage pinbarPage =new PinbarPage();
+
 
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
@@ -78,58 +78,7 @@ public class LoginStepDefs {
 
     }
 
- /*  @Then("the user should not be able to log in")
-    public void theUserShouldNotBeAbleToLogIn() {
 
-        BrowserUtils.verifyTitle("Login");
 
-    }
-*/
 
-    @Given("User is on the homepage")
-    public void userIsOnTheHomepage() {
-        user_is_on_the_login_page();
-    }
-
-    @And("User clicks on any page on the home page")
-    public void userClicksOnAnyPageOnTheHomePage() {
-        Actions actions = new Actions(Driver.getDriver());
-        actions.moveToElement(pinbarPage.fleetIcon).pause(1000).perform();
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOf(pinbarPage.vehiclesPage));
-        pinbarPage.vehiclesPage.click();
-        BrowserUtils.waitFor(10);
-
-    }
-
-    @When("User clicks on the Pinbar icon")
-    public void userClicksOnThePinbarIcon() {
-
-        pinbarPage.pinbarIcon.click();
-    }
-
-    @Then("User sees desired page highlighted in the header section in yellow")
-    public void userSeesDesiredPageHighlightedInTheHeaderSectionInYellow() {
-
-         pinbarPage.highlightedHeaderInYellow.isDisplayed();
-    }
-
-    @When("User hovers on that specific page")
-    public void userHoversOnThatSpecificPage() {
-   // Actions actions = new Actions(Driver.getDriver());
-    vytrackLoginPage.login("user1","UserUser123");
-     //   vytrackLoginPage.loginBtn.click();
-     //   BrowserUtils.waitFor(10);
-        pinbarPage.fleetIcon.isEnabled();
-
-    }
-
-    @Then("User sees expected image source")
-    public void userSeesExpectedImageSource() {
-    String actualImage = Driver.getDriver().getCurrentUrl();
-    //BrowserUtils.waitFor(5);
-    String expectedImage = "https://qa1.vytrack.com/entity/Extend_Entity_Carreservation?restore=1";
-    // Assertion
-    Assert.assertTrue(expectedImage.contains(actualImage));
-    }
 }
